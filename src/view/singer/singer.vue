56384d2ed1338e0e62fa732b9d556da5a400c23f
yaobo <1195022367@qq.com>
2018-11-15 20:51:21 +0800

    增加歌手页面

--- src/view/singer/singer.vue
+++ src/view/singer/singer.vue
@@ -1,56 +1,124 @@
 <template>
-  <div class = "singer">
-  
-    <div class = "singer-content">
+  <div class="singer">
 
-      <ul >
+    <scroll class = "scroll-content" :data = "singerList">
 
-      <li class = "c-li" v-for = "(item, index) of singerList" :key = "index" >
+      <div>
 
-        <p> {{item.name}}</p>
+        <ul class="singer-content">
 
-        <div>
+          <li class="c-li" v-for="(item, index) of singerList" :key="index">
 
-          <img :src  = "item.picUrl">
+            <p>
 
-          <p> {{item.name}}  </p>
+              <a :name="item.title">{{item.title}} </a>
 
-        </div>
+            </p>
 
-      </li>
+            <div v-for="sitem in item.items" :key="sitem.id">
 
-      </ul>
+              <img :src="sitem.avater">
+
+              <p> {{sitem.name}} </p>
+
+            </div>
+
+          </li>
+
+        </ul>
+
+      </div>
+
+    </scroll>
+
+    <div class="sortline">
+
+       <a v-for="(item, index) of singerList" :key="index" :href = "'#' + item.title" :class = "{active: activeChar.indexOf(item.title) >= 0} " @click = "activeChar = item.title" > {{item.title == "热门" ? "热" : item.title}} </a>
 
     </div>
-
   </div>
-
 </template>
 
 <script>
-  const pinyin = require("pinyin");
-  export default{
-    data () {
+  import scroll from '@/components/scroll/scroll'
+  const pinyin = require('pinyin');
+  export default {
+    components: {
+      scroll
+    },
+    data() {
       return {
-        singerList: []
+        singerList: [],
+        hotName: '热门',
+        activeChar: '热'
       }
     },
 
     created() {
       this.getSingerList();
     },
-    
+
     methods: {
       getSingerList() {
         let _this = this;
-        _this.axios('/top/artists?limit=100').then(function (response) {
+        _this.axios('/top/artists?limit=100').then(function(response) {
           let list = response.data.artists;
           list.map((item) => {
-            item.name[0], {
+            let py = pinyin(item.name[0], {
               style: pinyin.STYLE_FIRST_LETTER
+            })
+            item.initial = py[0][0].toUpperCase();
+          })
+          _this.singerList = _this._normalizeSinger(list);
+          console.log('singer:', _this.singerList);
+        })
+      },
+
+      _normalizeSinger(list) {
+        let map = {
+          hot: {
+            title: this.hotName,
+            items: []
+          }
+        };
+        list.forEach((item, index) => {
+          if (index < 10) {
+            map.hot.items.push({
+              id: item.id,
+              name: item.name,
+              avater: item.img1v1Url,
+              aliaName: item.alias[0]
+            })
+          }
+
+          const key = item.initial;
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
             }
+          }
+          map[key].items.push({
+            id: item.id,
+            name: item.name,
+            avater: item.img1v1Url,
+            aliaName: item.alias[0]
           })
         })
+        let hot = [];
+        let ret = [];
+        for (const key in map) {
+          let val = map[key];
+          if (val.title.match(/[A-Z/]/)) {
+            ret.push(val)
+          } else if (val.title === this.hotName) {
+            hot.push(val);
+          }
+        }
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0);
+        })
+        return hot.concat(ret);
       }
     }
   }
